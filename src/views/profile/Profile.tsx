a2a2206a3f3792736659a21194656035062a2649
sproutjeph <don_jeph@yahoo.com>
2022-11-20 07:13:30 +0000

    back Link on the profile page

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,7 +1,22 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const navigateTo = useNavigate();
+
+  return (
+    <>
+      <button
+        className="btn btn-primary ml-8 mt-4 py-[2px] px-4"
+        title="Back to Products"
+        onClick={() => navigateTo("../")}
+      >
+        <ArrowLeftIcon className="h-6 w-6 text-white  cursor-pointer" />
+      </button>
+      <div>Profile</div>
+    </>
+  );
 };
 
 export default Profile;
